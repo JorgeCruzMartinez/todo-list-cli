13e755b4288737a378f3485d530f7a643a112877
Jorge Cruz Martínez <25137854+JorgeCruzMartinez@users.noreply.github.com>
2026-09-11 12:39:22 -0600

    refactor: aplicar principios SOLID (SRP, OCP, DIP) al proyecto
    - Crear interfaz ITaskRepository para desacoplar el almacenamiento de la UI (DIP, OCP).
    - Renombrar TaskManager a JsonTaskRepository enfocándolo solo en la persistencia de datos (SRP).
    - Extraer la lógica visual de Spectre.Console hacia ConsoleUserInterface (SRP).
    - Inyectar el repositorio en la interfaz de usuario a través del constructor.

---
+++ ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace Todo_List;
+public  interface ITaskRepository
+{
+    void AddTask (string title);
+    List<TaskItem> GetAllTasks();
+    void ToggleTaskStatus (int id);
+    void DeleteTask (int id);
+    void UpdateTaskTitle (int id, string newTitle);
+}
